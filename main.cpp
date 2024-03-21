28bdf41f0ac3b1fff3d709e78fdba039dcc149e5
ming <asd0389wsx0216@gmail.com>
2024-03-21 19:02:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     for ( int i = 0; i < 10; i++ ) {
-        cout <<  "hi";
+        cout <<  "hi hello world";
     }
     return 0;
 }
