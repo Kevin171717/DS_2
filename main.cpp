44504442aa2591ad263fbf4640ebc08a0f472726
Kevin171717 <kevin47338647@gmail.com>
2024-03-21 18:40:51 +0800

    0.0.0.1
    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    for ( int i = 0; i < 10; i++ ) {
+        cout <<  "hi";
+    }
     return 0;
 }
