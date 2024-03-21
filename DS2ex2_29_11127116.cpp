0ff30ad1bb652a7f9ca8bdd8f8acf88bbfcbf832
Kevin171717 <kevin47338647@gmail.com>
2024-03-21 19:34:35 +0800

    0.0.0.1

--- DS2ex2_29_11127116.cpp
+++ DS2ex2_29_11127116.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
-<<<<<<< HEAD
     for ( int i = 0; i < 10; i++ ) {
-        cout <<  "hi hello world 10942208";
     }
-=======
->>>>>>> parent of 4450444 (0.0.0.1)
     return 0;
 }
